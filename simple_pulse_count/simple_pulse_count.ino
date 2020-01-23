509f1084484d9a0bf696f0a78bc9e1b5870e868f
lkdhruw <lkdhruw@live.com>
2020-01-23 17:53:34 +0530

    Add files via upload

---
+++ simple_pulse_count/simple_pulse_count.ino
@@ -0,0 +1,18 @@
+unsigned int count;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(2), countfn, CHANGE);
+}
+
+void countfn()
+{
+  count++;
+  Serial.print("Pulse Count ");
+  Serial.println(count);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
